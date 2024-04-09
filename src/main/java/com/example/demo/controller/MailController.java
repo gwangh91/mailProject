fcdb967319b869c1d3895866e032488e4219548b
gwangh91 <gwangh91@gmail.com>
2024-04-09 14:56:20 +0900

    Literal文字列を意味が明確な定数に変更 対応

--- src/main/java/com/example/demo/controller/MailController.java
+++ src/main/java/com/example/demo/controller/MailController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.service.MailService;
+import com.example.demo.util.Constants;
 
 @Controller
 public class MailController {
@@ -18,23 +19,23 @@ public class MailController {
 	
 	@GetMapping("/mailForm")
 	public String showMailForm() {
-		return "mailForm";
+		return Constants.MAIL_FORM_VIEW;
 	}
 	
-	@PostMapping("/confrimMailForm")
+	@PostMapping("/confirmMailForm")
 	public String confrimMailForm(String to, String subject, String body, Model model) {
 		
 		model.addAttribute("to", to);
 		model.addAttribute("subject", subject);
 		model.addAttribute("body", body);
 		
-		return "confrimMailForm";
+		return Constants.CONFIRM_MAIL_FORM_VIEW;
 	}
 
 	@PostMapping("/sendMail")
 	public String sendMail(String to, String subject, String body, Model model) {
 		mailService.sendEmail(to, subject, body);
-		model.addAttribute("message", "転送成功");
-		return "mailResult";
+		model.addAttribute("message", Constants.SUCCESS_MAIL_MESSAGE);
+		return Constants.MAIL_RESULT_VIEW;
 	}
 }
