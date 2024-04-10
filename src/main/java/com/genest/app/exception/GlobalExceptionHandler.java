25c1551105443ebb1f4533627c932bcd0489915c
gwangh91 <gwangh91@gmail.com>
2024-04-10 14:01:01 +0900

    エラーハンドリング機能、画面を作成

---
+++ src/main/java/com/genest/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.genest.app.exception;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.genest.app.util.Constants;
+
+@Controller
+public class GlobalExceptionHandler implements ErrorController {
+
+	@GetMapping(Constants.ERROR_PATH)
+	public String handleError(Model model) {
+		model.addAttribute("message", Constants.ERROR_MESSAGE);
+		return Constants.ERROR_VIEW;
+	}
+
+}
--- src/main/java/com/genest/app/util/Constants.java
+++ src/main/java/com/genest/app/util/Constants.java
@@ -5,7 +5,11 @@ public class Constants {
 	public static final String CONFIRM_MAIL_FORM_VIEW = "confirmMailForm";
 	public static final String MAIL_RESULT_VIEW = "mailResult";
 	public static final String MAIL_ERROR_VIEW = "mailError";
+	public static final String ERROR_VIEW = "errorForm";
+	
+	public static final String ERROR_PATH = "error";
 
 	public static final String SUCCESS_MAIL_MESSAGE = "転送成功";
 	public static final String ERROR_MAIL_MESSAGE = "メール送信中にエラーが発生しました。";
+	public static final String ERROR_MESSAGE = "ページが見つかりませんでした。";
 }
