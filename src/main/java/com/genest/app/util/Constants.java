5f4b29f2e6a52b92b154ceaf823023bc180bf106
gwangh91 <gwangh91@gmail.com>
2024-04-11 17:49:36 +0900

    エラーハンドリング機能、画面を作成

--- src/main/java/com/genest/app/util/Constants.java
+++ src/main/java/com/genest/app/util/Constants.java
@@ -4,6 +4,10 @@ public class Constants {
 	public static final String MAIL_FORM_VIEW = "mailForm";
 	public static final String CONFIRM_MAIL_FORM_VIEW = "confirmMailForm";
 	public static final String MAIL_RESULT_VIEW = "mailResult";
+	public static final String ERROR_VIEW = "errorForm";
+
+	public static final String ERROR_PATH = "error";
 
 	public static final String SUCCESS_MAIL_MESSAGE = "転送成功";
+	public static final String ERROR_MESSAGE = "ページが見つかりませんでした。";
 }
