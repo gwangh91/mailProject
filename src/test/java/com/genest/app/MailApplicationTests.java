cd404ae2337d878d1374420a11063e64eedaef26
gwangh91 <gwangh91@gmail.com>
2024-04-11 15:08:27 +0900

    controller, serviceのjunitテストコードを作成

--- src/test/java/com/genest/app/MailApplicationTests.java
+++ src/test/java/com/genest/app/MailApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.genest.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstProjectApplicationTests {
+class MailApplicationTests {
 
 	@Test
 	void contextLoads() {
